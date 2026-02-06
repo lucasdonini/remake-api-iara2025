40aeb5a75b4fba0a2d6308bc0ad8e4e476a26551
Lucas Kluska Donini <lkd.lucas@gmail.com>
2026-02-05 22:52:27 -0300

    init: Inital project scaffolding

---
+++ src/main/kotlin/com/interdisciplinar2025/remake/RemakeInter2025Application.kt
@@ -0,0 +1,11 @@
+package com.interdisciplinar2025.remake
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RemakeInter2025Application
+
+fun main(args: Array<String>) {
+    runApplication<RemakeInter2025Application>(*args)
+}
---
+++ src/test/kotlin/com/interdisciplinar2025/remake/RemakeInter2025ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.interdisciplinar2025.remake
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RemakeInter2025ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
