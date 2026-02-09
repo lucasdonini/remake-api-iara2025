b99fd01483381dcc8d6942b1d88e8ba3731f0015
Lucas Kluska Donini <lkd.lucas@gmail.com>
2026-02-09 17:46:52 -0300

    feat(plan): add Plan entity

---
+++ src/main/kotlin/com/interdisciplinar2025/remake/domain/Plan.kt
@@ -0,0 +1,30 @@
+package com.interdisciplinar2025.remake.domain
+
+import java.time.Period
+
+class Plan private constructor(
+    val name: String,
+    val price: Double,
+    val description: String?,
+    val duration: Period
+) : AggregateRoot() {
+    init {
+        require(name.isNotBlank()) { "Plan's name cannot be blank" }
+        require(price >= 0) { "Plan's price cannot be negative" }
+        require(duration.years > 0 || duration.months > 0) { "Plan's duration must be of at least one month" }
+    }
+
+    companion object {
+        fun create(
+            name: String,
+            price: Double,
+            description: String?,
+            duration: Period
+        ): Plan = Plan(
+            name = name,
+            price = price,
+            description = description,
+            duration = duration
+        )
+    }
+}
---
+++ src/test/kotlin/com/interdisciplinar2025/remake/common/builders/PlanBuilder.kt
@@ -0,0 +1,22 @@
+package com.interdisciplinar2025.remake.common.builders
+
+import com.interdisciplinar2025.remake.domain.Plan
+import java.time.Period
+
+class PlanBuilder private constructor() {
+    private var name: String = "plan"
+    private var price: Double = 10.0
+    private var description: String? = null
+    private var duration: Period = Period.ofMonths(1)
+
+    fun build(): Plan = Plan.create(
+        name = name,
+        price = price,
+        description = description,
+        duration = duration
+    )
+
+    companion object {
+        fun builder(): PlanBuilder = PlanBuilder()
+    }
+}
