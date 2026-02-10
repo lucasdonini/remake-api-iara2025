33a927e9a834e565c68516e6c17cbe10ed3510ba
Lucas Kluska Donini <lkd.lucas@gmail.com>
2026-02-10 16:56:27 -0300

    feat(aggregate root): implement equals() and hashCode()

--- src/main/kotlin/com/interdisciplinar2025/remake/domain/model/AggregateRoot.kt
+++ src/main/kotlin/com/interdisciplinar2025/remake/domain/model/AggregateRoot.kt
@@ -5,4 +5,14 @@ import java.util.UUID
 
 abstract class AggregateRoot {
     val id: UUID = EMPTY_UUID
+
+    final override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || this::class != other::class) return false
+
+        other as AggregateRoot
+        return id != EMPTY_UUID && id == other.id
+    }
+
+    final override fun hashCode(): Int = id.hashCode()
 }
