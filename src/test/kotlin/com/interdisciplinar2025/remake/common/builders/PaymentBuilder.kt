5655b365a3c65d730cb70b9dfdb9885d820d64c6
Lucas Kluska Donini <lkd.lucas@gmail.com>
2026-02-10 07:49:16 -0300

    feat(payment): add Payment entity

---
+++ src/test/kotlin/com/interdisciplinar2025/remake/common/builders/PaymentBuilder.kt
@@ -0,0 +1,39 @@
+package com.interdisciplinar2025.remake.common.builders
+
+import com.interdisciplinar2025.remake.domain.model.Payment
+import com.interdisciplinar2025.remake.domain.model.Payment.PaymentMethod
+import com.interdisciplinar2025.remake.domain.model.Payment.PaymentStatus
+import java.time.LocalDateTime
+import java.util.*
+
+class PaymentBuilder private constructor() {
+    private var value: Double = 10.0
+    private var status: PaymentStatus = PaymentStatus.PENDING
+    private var dueDate: LocalDateTime = LocalDateTime.now().plusMonths(1)
+    private var paymentDate: LocalDateTime? = null
+    private var method: PaymentMethod = PaymentMethod.PIX
+    private var payerFactoryId: UUID = UUID.randomUUID()
+    private var paidPlanId: UUID = UUID.randomUUID()
+
+    fun withValue(value: Double) = apply { this.value = value }
+    fun withStatus(value: PaymentStatus) = apply { status = value }
+    fun withDueDate(value: LocalDateTime) = apply { dueDate = value }
+    fun withPaymentDate(value: LocalDateTime?) = apply { paymentDate = value }
+    fun withMethod(value: PaymentMethod) = apply { method = value }
+    fun withPayerFactoryId(value: UUID) = apply { payerFactoryId = value }
+    fun withPaidPlanId(value: UUID) = apply { paidPlanId = value }
+
+    fun build(): Payment = Payment.create(
+        value = value,
+        status = status,
+        dueDate = dueDate,
+        paymentDate = paymentDate,
+        method = method,
+        payerFactoryId = payerFactoryId,
+        paidPlanId = paidPlanId
+    )
+
+    companion object {
+        fun builder(): PaymentBuilder = PaymentBuilder()
+    }
+}
